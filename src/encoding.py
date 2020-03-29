1ca726d8d0d1e2b2b030f03e660addccd0314312
Olivier CHURLAUD <olivier@churlaud.com>
2020-03-29 13:50:30 +0200

    Read all elements but with too much nesting

--- src/encoding.py
+++ src/encoding.py
@@ -1,5 +1,7 @@
 import pickle
 import xml.dom
+import re
+import sys
 
 from malpydefinitions import MALPY_ENCODING
 
@@ -58,20 +60,66 @@ class XMLEncoder(Encoder):
         rootElement.setAttributeNS(XML_NAMESPACE, XMLNS_XSI, XML_XSI_NAMESPACE_URL);
         rootElement.setAttributeNS(XML_NAMESPACE, MAL_XML, MAL_XML_NAMESPACE_URL);
 
-        for element in message:
+        for element in message.value:
             _encode_internal(element, rootElement)
         encodedmessage = d.toprettyxml(encoding="UTF-8")
         return encodedmessage
 
 
     def decode(self, message):
-        def _decode_internal(node, value):
-            pass
+        emptyNodePattern = re.compile(r"^[\n\t].*$")
+        """"
+                    /**
+            * Logic to remove extra spaces, tabs, and line-breaks.
+            * 1. Find all extra text elements.
+            * 2. if it has no parent or parent is ROOT, remove
+            * 2.1. if the parent has more than 1 elements, remove
+            *
+            * @param document XML Document
+            * @throws XPathExpressionException rquired for XPaths
+            */
+            private static void emptyNodeRemoval(Document document) throws XPathExpressionException {
+                XPathFactory xpathFactory = XPathFactory.newInstance();
+                // XPath to find empty text nodes.
+                XPathExpression xpathExp = xpathFactory.newXPath().compile(
+                        "//text()[normalize-space(.) = '']");
+                NodeList emptyTextNodes = (NodeList)
+                        xpathExp.evaluate(document, XPathConstants.NODESET);
 
+                // Remove each empty text node from document.
+                for (int i = 0; i < emptyTextNodes.getLength(); i++) {
+                    Node emptyTextNode = emptyTextNodes.item(i);
+                    Node parentNode = emptyTextNode.getParentNode();
+
+                    if (!(!Objects.isNull(parentNode) && !parentNode.getNodeName().equals(Constants.ROOT_ELEMENT) &&
+                            parentNode.getChildNodes().getLength() == 1))  {
+                        emptyTextNode.getParentNode().removeChild(emptyTextNode);
+                    }
+                }
+            }
+        """
+
+
+        def _decode_internal(node):
+            internal = []
+            for element in node.childNodes:
+                if element.nodeType is element.TEXT_NODE:
+                    if not re.match(emptyNodePattern, element.nodeValue):
+                        print("leaf TEXTE: {}".format(element.nodeValue))
+                        internal.append(element.nodeValue)
+                elif element.nodeType is element.ELEMENT_NODE:
+                    if element.hasAttribute('xsi:nil') and element.getAttribute('xsi:nil'):
+                        print("leaf ELSE: NULL")
+                        internal.append(None)
+                    else:
+                        internal.append(_decode_internal(element))
+                else:
+                    raise RuntimeError(element)
+            return internal
+
+        #clean_message = message.replace('\t', '').replace('\n', '')
         d = xml.dom.minidom.parseString(message)
         rootElement = d.firstChild
-        for element in rootElement.childNodes:
-            if element.nodeType is element.ELEMENT_NODE:
-                pass
-
-        return domdoc
+        value = _decode_internal(rootElement)
+        print(value)
+        return value
