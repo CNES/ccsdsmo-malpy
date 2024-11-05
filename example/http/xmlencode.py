fa0d1465c23e1cdd81db3224182305aa5049d999
Olivier Churlaud <olivier.churlaud@cnes.fr>
2024-11-06 00:02:27 +0100

    run xml encoder tests

--- example/http/xmlencode.py
+++ example/http/xmlencode.py
@@ -4,42 +4,8 @@ import sys
 sys.path.append('../../src/')
 import time
 
-from malpy.mo import mal
-from malpy.mo.mc.services import parameter
+from malpy.mo import mal, mc
 from malpy.encoding import xml
-
-#encoding.LOG_LEVEL = 'DEBUG'
-
-#m = mal.Subscription(
-#    ["MySubscription",
-#        [
-#            [
-#                ['Id1', 'Id2', None],
-#                True,
-#                True,
-#                True,
-#                True,
-#                [
-#                    ["IDK1", 9, None, None],
-#                    None
-#                ]
-#            ]
-#        ]
-#    ]
-#    )
-
-
-#u = mal.UpdateHeader(
-#    [
-#        123456.0,
- #       "http://localhost:80",
- #       mal.UpdateTypeEnum.CREATION,
- #       ["IDK1", 9, 1, 3]
- #   ])
-
-p= parameter.ParameterValue([mal.UOctet(5), mal.Blob(b'toto'), mal.String("Toto")])
-
-print("mllml", p._internal_value[2]._isNull)
 
 e = xml.XMLEncoder()
 
@@ -56,30 +22,65 @@ print(e.decode_body(em, mal.Time).internal_value)
 
 
 print("TEST: TimeList")
-
-em = e.encode_body(mal.TimeList([time.time(), time.time(), time.time()]))
+em = e.encode_body(mal.TimeList([time.time(), time.time()+5, time.time()+6]))
 print(em.decode('utf8'))
-print(e.decode_body(em, mal.Time))
+emm = e.decode_body(em, mal.TimeList)
+print(emm)
+print([x.internal_value for x in emm.internal_value])
 
-#em = e.encode_body([m])
-#print(e.decode_body(em, mal.Subscription))
-#print("{}".format(em.decode('utf-8')))
-#eu = e.encode_body([u])
-#print(e.decode_body(eu))
-#print("{}".format(eu.decode('utf-8')))
-#eul = e.encode_body([ul,u])
-#deul = e.decode_body(eul)
-#print(eul.decode('utf8'))
-#print(deul)
+print("TEST: Enum")
+em = e.encode_body([mal.InteractionType(mal.InteractionTypeEnum.SEND)])
+print(em.decode('utf8'))
+emm = e.decode_body(em, mal.InteractionType)
+print(emm.internal_value)
+
+
+print("TEST: Simple Composite")
+em = e.encode_body([mal.IdBooleanPair([mal.Identifier("TOTO"), False])])
+print(em.decode('utf8'))
+emm = e.decode_body(em, mal.IdBooleanPair)
+print(emm)
+print(emm.internal_value)
+
+p= mc.ParameterValueData([mc.ValidityState(mc.ValidityStateEnum.VALID), mal.Blob(b'toto'), mal.String("Toto")])
+
+print("mllml", p._internal_value[2]._isNull)
 
 coded  = e.encode_body([p])
 print(coded.decode('utf8'))
-decoded = e.decode_body(coded)
-print(decoded[0].internal_value[1].internal_value)
+decoded = e.decode_body(coded, mc.ParameterValueData)
+print(decoded)
+print([x.internal_value for x in decoded.internal_value])
 print(e.encode_body(decoded).decode('utf8'))
 
-import time
-p = parameter.ParameterValueDetailsList([
-    parameter.ParameterValueDetails([ 2, 2, time.time(), [mal.UOctet(0),mal.Long(1),None]])])
-pc = e.encode_body([p])
-print(pc.decode('utf8'))
+m = mal.Subscription([
+        "MySubscription",
+        ['fr', 'cnes', None],
+        None,
+        [
+            ['key1', [mal.Integer(1),mal.Integer(2),mal.Integer(3)]],
+            ['key2', [mal.Boolean(True), mal.Boolean(False)]]
+        ]
+    ])
+
+u = mal.UpdateHeader(
+    [
+        "CNES",
+        ['fr', 'cnes', 'swot'],
+        [
+            [mal.Boolean(True)],
+            [mal.Boolean(None)],
+            [mal.Float(2.3)]
+        ]
+    ])
+em = e.encode_body([m])
+print(em.decode('utf-8'))
+print(e.decode_body(em, mal.Subscription))
+print("{}".format(em.decode('utf-8')))
+eu = e.encode_body([u])
+print(e.decode_body(eu))
+print("{}".format(eu.decode('utf-8')))
+eul = e.encode_body([u,u])
+deul = e.decode_body(eul)
+print(eul.decode('utf8'))
+print(deul)
